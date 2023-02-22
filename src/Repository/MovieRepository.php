3c22427ae0ea4b8dd8e7bd885524913c35583430
Jean-ChristopheMartin <mjeanchristophe88@gmail.com>
2023-02-22 17:14:14 +0100

    doctrine extention querry builder rand object ok

--- src/Repository/MovieRepository.php
+++ src/Repository/MovieRepository.php
@@ -39,26 +39,36 @@ class MovieRepository extends ServiceEntityRepository
         }
     }
 
-    /**
-     * method that returns a random number(limit) of movies
-     *
-     * @param integer $limit
-     * @return array
-     */
-    public function findRandomMoviesGame(int $limit): array
+    // /**
+    //  * method that returns a random number(limit) of movies
+    //  *
+    //  * @param integer $limit
+    //  * @return array
+    //  */
+    // public function findRandomMoviesGame(int $limit): array
+    // {
+    //     $conn = $this->getEntityManager()->getConnection();
+
+    //     $sql = "
+    //         SELECT * FROM movie m
+    //         ORDER BY RAND()
+    //         LIMIT :limit
+    //         ";
+    //     $stmt = $conn->prepare($sql);
+    //     $stmt->bindValue('limit', intval($limit), \PDO::PARAM_INT);
+    //     $resultSet = $stmt->executeQuery();
+
+    //     return $resultSet->fetchAllAssociative();
+    // }
+
+    public function findRandomMoviesGame(int $limit)
     {
-        $conn = $this->getEntityManager()->getConnection();
-
-        $sql = "
-            SELECT * FROM movie m
-            ORDER BY RAND()
-            LIMIT :limit
-            ";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindValue('limit', intval($limit), \PDO::PARAM_INT);
-        $resultSet = $stmt->executeQuery();
-
-        return $resultSet->fetchAllAssociative();
+        return $this->createQueryBuilder('c')
+            ->orderBy('RAND()')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult()
+        ;
     }
 
 //    /**
