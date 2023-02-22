fedc8c27c48d81fa22b160ac1689a60941320ca3
Jean-ChristopheMartin <mjeanchristophe88@gmail.com>
2023-02-22 16:39:53 +0100

    Api/GameController ok

--- src/Controller/Api/GameController.php
+++ src/Controller/Api/GameController.php
@@ -4,10 +4,15 @@ namespace App\Controller\Api;
 
 use App\Entity\Game;
 use App\Repository\GameRepository;
+use App\Repository\MovieRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class GameController extends AbstractController
 {
@@ -44,8 +49,44 @@ class GameController extends AbstractController
      * @param Game $game
      * @return JsonResponse
      */
-    public function add(): JsonResponse
+    public function add(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, GameRepository $gameRepository, UserRepository $userRepository, MovieRepository $movieRepository): JsonResponse
     {
-        return $this->json($game, Response::HTTP_OK, [], ['groups' => 'games']);
+        $json = $request->getContent();
+
+        $game = $serializer->deserialize($json, Game::class, 'json');
+
+        $errors = $validator->validate($game);
+
+        if (count($errors)) {
+            $errorsArray = [];
+
+            foreach ($errors as $error) {
+                $errorsArray[$error->getPropertyPath()][] = $error->getMessage();
+            }
+
+            return $this->json($errorsArray,Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        // I get my request in a array
+        $content = $request->toArray();
+
+        // ! User
+
+        $userId = $content['userId'];
+        $game->setUser($userRepository->find($userId));
+
+        // ! Movie
+
+        $idMovies = $content['moviesId'];
+        foreach ($idMovies as $idMovie) {
+            $game->addMovie($movieRepository->find($idMovie));
+        }
+
+        $gameRepository->add($game, true);
+
+        return $this->json($game, Response::HTTP_CREATED, [
+            'location' => $this->generateUrl("app_api_movie_show", ["id" => $game->getId()])
+        ], 
+        ['groups' => 'games']);
     }
 }
